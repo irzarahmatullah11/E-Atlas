9ff52d3c3ed7b1e8b0626db6343736e94808d859
irzarahmatullah11 <145815209+irzarahmatullah11@users.noreply.github.com>
2026-01-04 15:49:04 +0700

    Update fbscript.js

--- js/fbscript.js
+++ js/fbscript.js
@@ -134,7 +134,7 @@ var callback = function (e){
             attributes.align = "middle";
             
             
-var hasMobileVersion=true;
+var hasMobileVersion=false;
 var hasBasicHtmlVersion=true;
 
 var callback = function (e){ 
